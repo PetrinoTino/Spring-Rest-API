b6866936446c705cfbec3aed0314f104ce6e1180
Petrino Qasolli <petrinotino@gmail.com>
2024-12-22 14:51:16 +0100

    item

--- Sda-Spring-Example-rest-Api/src/main/java/com/JavaTirane42/Sda_Spring_Example_rest_Api/entity/Item.java
+++ Sda-Spring-Example-rest-Api/src/main/java/com/JavaTirane42/Sda_Spring_Example_rest_Api/entity/Item.java
@@ -5,11 +5,9 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "Items")
 public class Item {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-
     private String name;
     private String description;
     private String category;
@@ -22,46 +20,35 @@ public class Item {
         this.category = category;
         this.price = price;
     }
-
     public Item() {
     }
-
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
     public String getCategory() {
         return category;
     }
-
     public void setCategory(String category) {
         this.category = category;
     }
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
