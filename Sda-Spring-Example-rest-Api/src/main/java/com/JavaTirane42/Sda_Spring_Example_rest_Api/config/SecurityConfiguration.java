43534a3a5c2041547c542645044128f92229d076
Petrino Qasolli <petrinotino@gmail.com>
2024-12-22 14:36:06 +0100

    seciurity

--- Sda-Spring-Example-rest-Api/src/main/java/com/JavaTirane42/Sda_Spring_Example_rest_Api/config/SecurityConfiguration.java
+++ Sda-Spring-Example-rest-Api/src/main/java/com/JavaTirane42/Sda_Spring_Example_rest_Api/config/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
